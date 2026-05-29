ceef4094bbac6905c91c032671966954b6539a3f
Cesare Naldi <3353250+cesarenaldi@users.noreply.github.com>
2026-05-29 14:48:40 +0200

    chore: release 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="py_builder_relayer_client",
-    version="0.0.2rc1",
+    version="0.0.2",
     author="Polymarket Engineering",
     author_email="engineering@polymarket.com",
     maintainer="Polymarket Engineering",
